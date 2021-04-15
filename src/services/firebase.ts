f44bc0f0e406ee416c66ade352487634f46db750
Andres Celis <adwher@outlook.com>
2021-04-15 15:56:45 -0500

    fix: update firebase keys to .env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,11 +2,11 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDW0h2CL1j2Bx2aMP12xfEH2iHj_7NBjJo",
-    authDomain: "nautilus-dev-60876.firebaseapp.com",
-    projectId: "nautilus-dev-60876",
-    storageBucket: "nautilus-dev-60876.appspot.com",
-    messagingSenderId: "886946351458",
-    appId: "1:886946351458:web:f19ad3b80bcd7fd607043b",
-    measurementId: "G-N8Z82ZQLT8"
+    apiKey: import.meta.env.VITE_FIREBASE_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER,
+    appId: import.meta.env.VITE_FIREBASE_APP,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASURE
 })
